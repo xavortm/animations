cd7935df6bf99028b6f1d73d261af256bd9f1a8c
Alex Dimitrov <alex.dimitrov@10up.com>
2022-03-09 15:31:32 +0200

    Fix error and improve typograpgy

--- components/explanation/index.js
+++ components/explanation/index.js
@@ -1,20 +1,15 @@
 import styled from "styled-components";
 
-const Explanation = ({children}) => {
-  return (
-    <ExplanationStyled>
-		{children}
-    </ExplanationStyled>
-  );
+const Explanation = ({ children }) => {
+  return <ExplanationStyled>{children}</ExplanationStyled>;
 };
 
-
-
 const ExplanationStyled = styled.div`
-  font-size: .875em;
+  font-size: 0.875em;
   font-family: var(--font-secondary);
   border-top: 1px dashed var(--color-border);
   margin-top: 2em;
+  line-height: 1.5;
 `;
 
 export default Explanation;
